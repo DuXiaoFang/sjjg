0749eb229cf495d51ac165354fda98dc96156842
DuXiaoFang <895789707@qq.com>
2021-12-02 22:33:53 +0800

    main

---
+++ leetcode/offer13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution{
+
+private:
+    int dfs(int i,int j,int si,int sj,int cou,vector<vector<bool>> dp,int m,int n,int k){
+        if(i >= m || j >= n || k < si+sj || dp[i][j]) return 0;
+        for(int ){
+            cou += 1;
+            
+
+        }
+    }
+public:
+    int movingCount(int m, int n, int k){
+        vector<vector<bool>> dp(m,vector<bool>(n));
+    }
+};
